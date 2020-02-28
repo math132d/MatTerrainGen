7dcf4e98283fa59b5332cbf7e412112e711755a0
Mathias <majohha@gmail.com>
2020-02-28 17:35:47 +0100

    Added interface to control terrain

--- src/index.js
+++ src/index.js
@@ -5,21 +5,37 @@ import Renderer from './Renderer';
 
 import { noise_to_canvas } from 'wasm';
 
+const SETTINGS = document.getElementById("window");
+const CLOSE_SETTINGS = document.getElementById("close_settings");
+const OPEN_SETTINGS = document.getElementById("open_settings");
 const CANVAS = document.getElementById("canvas");
 const HEIGHTMAP = document.getElementById("heightmap");
 const HEIGHTMAP_CTX = HEIGHTMAP.getContext("2d");
 
-const in_range = document.getElementById("range");
+const in_water = document.getElementById("tr_water");
+const in_range = document.getElementById("tr_height");
 const in_gen = document.getElementById("gen_hm");
 
+CLOSE_SETTINGS.addEventListener("click", () => {
+    toggle_settings();
+})
+
+OPEN_SETTINGS.addEventListener("click", () => {
+    toggle_settings();
+})
+
 in_gen.addEventListener("click", () => {
-    update_map(6, 4);
-    mainscene.terrainBuilder.update_texture();
+    update_map();
 })
 
 in_range.addEventListener("input", () => {
-    let value = in_range.value / 100;
+    let value = in_range.value / 1000;
     mainscene.terrainBuilder.get_mesh().material.uniforms.scale.value = value;
+})
+
+in_water-addEventListener("input", () => {
+    let value = in_water.value / 1000;
+    mainscene.water.position.y = value;
 })
 
 let WIDTH = window.innerWidth;
@@ -38,10 +54,10 @@ ASSETS.start_loading( () => {
 const init = function() {
     renderer = new Renderer(CANVAS, WIDTH, HEIGHT);
 
-    update_map(6, 4);
-
     mainscene = new MainScene(renderer);
     mainscene.init();
+
+    update_map();
 
     prevFrame = Date.now();
 
@@ -66,16 +82,30 @@ const resize = () => {
     mainscene.resize();
 }
 
-const update_map = (freq, oct) => {
-    const w = 512;
-    const h = 512;
+const update_map = () => {
+    const size = document.getElementById("hm_size").value;
+    const freq = document.getElementById("hm_freq").value;
+    const oct = document.getElementById("hm_oct").value;
 
-    console.log(`${w}, ${h}`);   
+    const w = size;
+    const h = size;
+
+    console.log(`${w}, ${h}`);
 
     let pixel_view = new Uint8ClampedArray(w * h * 4);
     noise_to_canvas(w, h, freq, oct, pixel_view);
     let imageData = new ImageData(pixel_view, w, h);
     HEIGHTMAP_CTX.putImageData(imageData, 0, 0);
+
+    mainscene.terrainBuilder.update_texture();
+}
+
+const toggle_settings = () => {
+    if( SETTINGS.hasAttribute("closed") ){
+        SETTINGS.removeAttribute("closed");
+    }else{
+        SETTINGS.setAttribute("closed", "true");
+    }
 }
 
 window.onresize = resize;
